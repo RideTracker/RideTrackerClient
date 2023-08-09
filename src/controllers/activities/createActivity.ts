da4227acfd77591b94b9cdc7ae63d6c83bc42f45
Nora Söderlund <norasoderlund@icloud.com>
2023-08-09 15:03:02 +0200

    Updated createActivity controller

--- src/controllers/activities/createActivity.ts
+++ src/controllers/activities/createActivity.ts
@@ -1,5 +1,6 @@
 import Client from "../..";
 import { DefaultResponse } from "../../models/DefaultResponse";
+import { Recording } from "@ridetracker/ridetrackertypes";
 
 export type CreateActivityResponse = DefaultResponse & {
     activity: {
@@ -7,12 +8,11 @@ export type CreateActivityResponse = DefaultResponse & {
     };
 };
 
-export async function createActivity(client: Client, localId: string, sessions: any, visibility: string): Promise<CreateActivityResponse> {
+export async function createActivity(client: Client, recording: Recording, visibility: string): Promise<CreateActivityResponse> {
     const url = new URL(`${client.host}/api/activities/create`);
 
     const body = {
-        localId,
-        sessions,
+        recording,
         visibility
     };
 
